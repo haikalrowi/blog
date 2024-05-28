38f6bf372338166f89f04197821ebe249dcc4b04
Haikal Rowi <93899076+haikalrowi@users.noreply.github.com>
2024-05-28 10:59:58 +0700

    prisma

---
+++ prisma/seed.js
@@ -0,0 +1,39 @@
+// @ts-check
+
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs");
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const password = bcrypt.hashSync("password");
+  const alice = await prisma.user.upsert({
+    where: { email: "alice@prisma.io" },
+    update: {},
+    create: {
+      email: "alice@prisma.io",
+      name: "Alice",
+      Password: { create: { password } },
+    },
+  });
+  const bob = await prisma.user.upsert({
+    where: { email: "bob@prisma.io" },
+    update: {},
+    create: {
+      email: "bob@prisma.io",
+      name: "Bob",
+      Password: { create: { password } },
+    },
+  });
+  console.log({ alice, bob });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
